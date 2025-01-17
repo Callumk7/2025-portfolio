7080d6d8627f77b44b907866c2c612ce3d0f1677
Callum Kloos <Callumkloos@gmail.com>
2025-01-17 15:28:00 +0000

    contact page and more structure

--- src/components/home/interactive/project-picker.tsx
+++ src/components/home/interactive/project-picker.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 
 interface ProjectPickerProps {
 	projects: Project[];
@@ -13,10 +13,27 @@ interface Project {
 export function ProjectPicker(props: ProjectPickerProps) {
 	const [selected, setSelected] = createSignal<Project>(props.projects[0]);
 	return (
-		<div>
-			<h1>Hello</h1>
-			<div>
-				Selected project title: <span class="text-warning bg-dark">{selected().title}</span>
+		<div class="grid-2">
+			<div class="d-flex flex-column border-right divide-y">
+				<For each={props.projects}>
+					{(project) => (
+						<button
+							type="button"
+							onClick={() => setSelected(project)}
+							classList={{
+								"slider-selected text-left": selected().id === project.id,
+								"slider text-left": selected().id !== project.id,
+							}}
+						>
+							<h2>{project.title}</h2>
+						</button>
+					)}
+				</For>
+			</div>
+			<div class="p-3 d-flex flex-column justify-content-stretch h-100">
+				<h2>{selected().title}</h2>
+				<p>{selected().description}</p>
+				<a href="/place">Read more.</a>
 			</div>
 		</div>
 	);
