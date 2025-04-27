f325c2a86f845ee9d7e0a0c454767f0eb00ceb92
Callum Kloos <Callumkloos@gmail.com>
2025-04-27 15:58:56 +0100

    wip - work on unifying the styles

--- src/components/interactive/project-view.tsx
+++ src/components/interactive/project-view.tsx
@@ -1,11 +1,9 @@
 import type { Project } from "../../types";
-import styles from "./project-view.module.scss";
-import heightStyles from "./project-picker.module.scss";
 import { Tags } from "./tags";
 
 export function ProjectView(props: { project: Project }) {
 	return (
-		<div class={`${styles.container} ${heightStyles.minHeight} h-100`}>
+		<div class="content-preview">
 			<div>
 				<h2>{props.project.name}</h2>
 				<Tags tags={props.project.tags} />
